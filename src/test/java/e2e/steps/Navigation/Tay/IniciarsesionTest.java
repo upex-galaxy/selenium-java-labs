0964c29f8ade8a5bf798f8b9728e2415c33786fd
ramostaime <ramostaime@gmail.com>
2023-11-07 09:58:41 -0300

    doc(Sanity/Iniciarsesion):cambios generales

---
+++ src/test/java/e2e/steps/Navigation/Tay/IniciarsesionTest.java
@@ -0,0 +1,12 @@
+package e2e.steps.Navigation.Tay;
+
+import e2e.*;
+import org.junit.jupiter.api.*;
+
+public class IniciarsesionTest {
+    // @Test
+    // Declaracion de variables
+
+    // Declaracion de metodos
+
+}
